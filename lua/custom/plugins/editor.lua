a37d03005418ba83d748a7252e019ff9722dcc2e
Matt Hull <matt@userevidence.com>
2024-04-27 21:31:16 -0500

    modularize all

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -1,4 +1,8 @@
 return {
+  -- detect tabstop automatically
+  { 'tpope/vim-sleuth' },
+  -- gc to comment visual ranges
+  { 'numToStr/Comment.nvim' },
   {
     'ggandor/leap.nvim',
     event = 'VeryLazy',
@@ -31,6 +35,17 @@ return {
         highlight = 'gsh',
         replace = 'gsr',
         update_n_lines = 'gsn',
+      },
+    },
+  },
+
+  {
+    'echasnovski/mini.ai',
+    opts = {
+      n_lines = 500,
+      mappings = {
+        goto_left = 'gh',
+        goto_right = 'gl',
       },
     },
   },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'duskfox'
+    end,
+  },
+}
